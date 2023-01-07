0a598f1f86bfb9cbe34f4431d9a2dccf6b577ee6
Alena <alena.teniakova@gmail.com>
2023-01-07 11:54:10 +0700

    hw

---
+++ task_41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите несколько чисел через пробел: ");
+int[] array = GetArray(Console.ReadLine()!);
+int positiveNumber = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        positiveNumber++;
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля: {positiveNumber}");
+
+
+int[] GetArray(string input)
+{
+    int count = 1;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    int[] array = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ' ')
+        {
+        if (i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        array[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return array;
+}
---
+++ task_42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 42: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = Math.Round((-b2 + b1)/(-k1 + k2), 2);
+double y = Math.Round(k2 * x + b2, 2);
+
+Console.Write($"Координаты точки пересечения двух прямых - X: {x}, Y: {y}");
